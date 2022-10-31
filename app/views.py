b7867e8f55b89b537a5fcfa19f7b348d1e8cdf85
SheetalJade2019 <sheetal.jade93@gmail.com>
2022-10-31 21:18:33 +0530

    SQLAlchemy code adde

--- app/views.py
+++ app/views.py
@@ -6,35 +6,71 @@ from django.core.files.storage import FileSystemStorage
 from project.settings import BASE_DIR
 from django.views.decorators.csrf import csrf_exempt
 import pandas as pd
+import sqlite3
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
 
-
-
-# Create your views here.
 @csrf_exempt
 def upload_file(request):
     if request.method == 'POST' and request.FILES['myfile']:
+        # get input data from request & store it to media folder
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
-        
         filename = fs.save(f"{myfile.name}", myfile)
         uploaded_file_url = fs.url(filename)
+        uploaded_file_url = f"{BASE_DIR}\media\{myfile}"
         print("uploaded_file_url ",uploaded_file_url)
-        df = pd.read_excel(f"{BASE_DIR}\media\{myfile}", engine="openpyxl")
-        # print(df.head())
 
-        column_names = list(df.columns)
-        print("column_names : ",column_names)
-        unique_val = df["Category"].unique()
-        print("unique_val ",unique_val)
-        new_df = pd.read_excel("D:\SHEETAL\LEARN\INTERVIEWS\TechMahindra\project\media\Input.xlsx", engine="openpyxl", index_col="Category")
-        new_col = new_df.columns
-        for cv in unique_val:
-            print(cv)
-            temp_df = new_df.loc[cv]
-            df_temp = pd.DataFrame(temp_df,columns=new_col)
-            df_temp.loc[len(df_temp.index)] = df_temp[new_col].sum()
-            df_temp.to_excel ('export_dataframe.xlsx', index = False, header=True)
-            print(df_temp)
+        # read csv and create dataframe
+        df = pd.read_excel(f"{BASE_DIR}\media\{myfile}", engine="openpyxl")
+        print("\n 3 rows of input file \n",df.head(3))
+
+        # database connection
+        my_path=f"{BASE_DIR}\db.sqlite3" # update path
+        my_conn = sqlite3.connect(my_path)
+        print("Connected to database successfully")
+        my_conn = create_engine("sqlite:///"+ my_path) # connection object
+
+        try:
+            column_ls = df.columns
+            # create SQLite db table Input.
+            df.to_sql(con=my_conn,name='Input',if_exists='append') 
+            # print("Worked")
+            # query to collect record
+            query="SELECT * FROM Input"  
+            df = pd.read_sql(query,my_conn,columns= column_ls) # create DataFrame
+            df = df.sort_values(by=['Category'])
+            # df = df.drop(['index'], axis=1)
+            print("\n------  top 3 rows of dataframe  -------\n",df.head(3)) # Print top 5 rows as sample
+            unique_val = df["Category"].unique()
+            df = [x for _, x in df.groupby(df['Category'])]
+            print("len(df) : ",len(df))
+            df = pd.DataFrame(df)
+            # print(df.head(2))
+            # df_temp = pd.DataFrame(columns = column_ls)
+            # for r in df.iterrows():
+            #     # for i in unique_val[:-1]:
+            #     print(r[0] , r[1])
+            #     if r[1] is unique_val[0]:
+            #         df_temp = pd.DataFrame(r,columns = column_ls)
+            #         df_temp.loc[len(df_temp.index)] = df_temp[new_col].sum()
+            # print(df_temp.head())
+                # temp_df.to_excel("temp_df.xlsx", index=True)
+
+            # save result to excel
+            df.to_excel(f"{BASE_DIR}\media\output\{myfile}_result.xlsx", index=True)
+        except SQLAlchemyError as e:
+            print(e)
+            error = str(e.__dict__['orig'])
+            print(error)
+        else: 
+            pass
+            # r_set=my_conn.execute('DELETE from Input');
+            # print("deleted")
+            # for row in r_set:
+            #     print(row)
+
+
 
         return render(request, 'upload.html', {
             'uploaded_file_url': uploaded_file_url
